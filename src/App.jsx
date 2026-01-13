f537967517305e5ef5c66f97f2d23d01322330dd
himelmahmud <soft.mahmud@gmail.com>
2026-01-13 21:40:06 +0600

    ui completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Header from "./Components/header/header";
+import WeatherBoard from "./Components/weather/WeatherBoard";
 
 function App() {
-    return (
-        <>
-            <Header />
-        </>
-    );
+  return (
+    <div className="grid place-items-center h-screen">
+      <Header />
+      <main>
+        <section>
+          <WeatherBoard />
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default App;
